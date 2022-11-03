e83748b2c84031ace5f16246f8b991c5af65ad9e
kaoru <s21c1122az@s.chibakoudai.jp>
2022-11-03 12:31:19 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,24 +1,15 @@
 #!/bin/bash -xv
-#SPDX-FileCopyrightText: 2022 Miyazawa kaoeu
-#SPDX -Licensa -Identifier:BSD -3 -Clause　
-ng () { 
-      echo NG at Line $1
-      res=1 
-}
-
-res=0 ### I/O TEST ###
-  ### I/O ###
-out=$(seq 5 | ./plus)
-
-[ "${out}" = 14 ] || ng ${LINENO}        　 
-  ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-[ "$res" = 0 ] && echo OK
-exit $res
-
+#SPDX-FileCopyrightTeo NG at Line $1
+ng () {
+	      echo NG at Line $1
+	            res=1
+	    }
+    　
+    res=0
+    　
+    ### I/O TEST ###
+    out=$(seq 5 | ./plus)
+    [ "${out}" = 14 ] || ng ${LINENO}
+    　
+    [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+    exit $res
