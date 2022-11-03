f03911ba9ba273ff37a323014e6c9c49bc441956
kaoru <s21c1122az@s.chibakoudai.jp>
2022-11-03 12:32:44 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,15 +1,1 @@
-#!/bin/bash -xv
-#SPDX-FileCopyrightTeo NG at Line $1
-ng () {
-	      echo NG at Line $1
-	            res=1
-	    }
-    　
-    res=0
-    　
-    ### I/O TEST ###
-    out=$(seq 5 | ./plus)
-    [ "${out}" = 14 ] || ng ${LINENO}
-    　
-    [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
-    exit $res
+
