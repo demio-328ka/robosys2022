114380ab031c7f48aa940dc73ccddaa60dc4ac3f
kaoru <s21c1122az@s.chibakoudai.jp>
2022-11-03 12:24:19 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,21 +1,24 @@
 #!/bin/bash -xv
 #SPDX-FileCopyrightText: 2022 Miyazawa kaoeu
 #SPDX -Licensa -Identifier:BSD -3 -Clause　
-ng () {  echo NG at Line $1
-         res=1 
+ng () { 
+      echo NG at Line $1
+      res=1 
 }
 
 res=0 ### I/O TEST ###
   ### I/O ###
- out=$(seq 5 | ./plus)
- [ "${out}" = 15 ] || ng ${LINENO}        　 
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}        　 
   ### STRANGE INPUT ###
- out=$(echo あ | ./plus)
- [ "$?" = 1 ]      || ng ${LINENO}
- [ "${out}" = "" ] || ng ${LINENO}
+out=$(echo あ | ./plus)
+
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 out=$(echo | ./plus) #空文字
- [ "$?" = 1 ]      || ng ${LINENO}
- [ "${out}" = "" ] || ng ${LINENO}
- [ "$res" = 0 ] && echo OK
- exit $res
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK
+exit $res
 
