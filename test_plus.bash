d154a4863aea9272d33a1228de3a97610dd51db5
kaoru <s21c1122az@s.chibakoudai.jp>
2022-12-29 11:08:54 +0900

    Add a command

---
+++ test_plus.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Miyazawa Kaoru
+# SPDX-License-Identifier: BSD-3-Clause
+ng () {
+       echo NG at Line $1
+       res=1
+}
+
+res=0
+
+ ### I/O TESt ###
+ out=$(seq 5 |./plus)
+ [ "${out}" = 3.0 ] || ng ${LINENO} #15
+ out=$(echo あ |./plus)
+ [ "${out}" = "0.0" ] || ng ${LINENO}
+ out=$( echo   |./plus)
+ [ "${out}" = "0.0" ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK
+ exit $res
+
+
